cdb0c57e10e4b5456276707c2950b5873495d853
Sarah Scheffler <sscheff@bu.edu>
2017-04-26 12:06:23 -0400

    makesymlinks also downloads molokai now

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -7,6 +7,7 @@ FILES="bashrc vimrc tmux.conf"
 
 mkdir -p $BACKUP # if backup folder doesn't exist, make it
 
+# Move old dotfiles to backup directory and symlink these ones to ~
 cd $DOTFILES
 for file in $FILES; do
     if [ -e ~/.$file ] ; then
@@ -14,4 +15,10 @@ for file in $FILES; do
     fi
     ln -sfv $DOTFILES/.$file ~/.$file
 done
+
+# Download molokai color scheme for vim
+mkdir -p ~/.vim/colors
+wget https://github.com/tomasr/molokai/blob/master/colors/molokai.vim -P ~/.vim/colors
+
+source ~/.bashrc
 
