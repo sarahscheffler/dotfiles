37acaaa19f89f172fc42ae721560a0fe73c7f9ad
Sarah Scheffler <sscheffler@hmc.edu>
2017-04-27 10:24:54 -0400

    only download color scheme if we don't already have it

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -5,20 +5,25 @@ DOTFILES=~/dotfiles
 BACKUP=~/backup_dotfiles
 FILES="bashrc vimrc tmux.conf"
 
+VIMCOLORS=~/.vim/colors
+COLORSCHEME=https://github.com/tomasr/molokai/blob/master/colors/molokai.vim
+
 mkdir -p $BACKUP # if backup folder doesn't exist, make it
 
 # Move old dotfiles to backup directory and symlink these ones to ~
 cd $DOTFILES
 for file in $FILES; do
-    if [ -e ~/.$file ] ; then
-        mv ~/.$file $BACKUP
+    if [ -f "~/.$file" ] ; then
+        mv "~/.$file" "$BACKUP"
     fi
-    ln -sfv $DOTFILES/.$file ~/.$file
+    ln -sfv "$DOTFILES/.$file" "~/.$file"
 done
 
-# Download molokai color scheme for vim
-mkdir -p ~/.vim/colors
-wget https://github.com/tomasr/molokai/blob/master/colors/molokai.vim -P ~/.vim/colors
+# Download molokai color scheme for vim if we don't already have it
+mkdir -p "$VIMCOLORS"
+[ -f "$VIMCOLORS/${COLORSCHEME##*/}" ] || wget "$COLORSCHEME" -P "$VIMCOLORS"
 
+# Reload configs
 source ~/.bashrc
+tmux source-file ~/.tmux.conf
 
