1bca5394343a014b2e550ae677e467b4fd9e860b
Sarah Scheffler <sscheffler@hmc.edu>
2017-06-05 17:07:23 -0400

    Use '/Users/firechant' instead of '~' in script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,29 +1,30 @@
-# Copy all files in FILES from ~ to BACKUP, then create symlinks
+# Copy all files in FILES from $HOME to BACKUP, then create symlinks
 # from the home directory to these files
 
-DOTFILES=~/dotfiles
-BACKUP=~/backup_dotfiles
+DOTFILES=$HOME/dotfiles
+BACKUP=$HOME/backup_dotfiles
 FILES="bashrc vimrc tmux.conf"
 
-VIMCOLORS=~/.vim/colors
+VIMCOLORS=$HOME/.vim/colors
 COLORSCHEME=https://github.com/tomasr/molokai/blob/master/colors/molokai.vim
 
 mkdir -p $BACKUP # if backup folder doesn't exist, make it
 
-# Move old dotfiles to backup directory and symlink these ones to ~
+# Move old dotfiles to backup directory and symlink these ones to $HOME
 cd $DOTFILES
 for file in $FILES; do
-    if [ -f "~/.$file" ] ; then
-        mv "~/.$file" "$BACKUP"
+    if [ -f "$HOME/.$file" ] ; then
+        mv "$HOME/.$file" "$BACKUP"
     fi
-    ln -sfv "$DOTFILES/.$file" "~/.$file"
+    ln -sfv "$DOTFILES/.$file" "$HOME/.$file"
 done
 
 # Download molokai color scheme for vim if we don't already have it
 mkdir -p "$VIMCOLORS"
+#TODO this gets the html page, not the actual raw!!!
 [ -f "$VIMCOLORS/${COLORSCHEME##*/}" ] || wget "$COLORSCHEME" -P "$VIMCOLORS"
 
 # Reload configs
-source ~/.bashrc
-tmux source-file ~/.tmux.conf
+source $HOME/.bashrc
+tmux source-file $HOME/.tmux.conf
 
