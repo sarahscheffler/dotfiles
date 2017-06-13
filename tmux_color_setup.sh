09f1c6e73885fc394beb5343c4c53d618cb2e2db
Sarah Scheffler <sscheffler@hmc.edu>
2017-06-13 10:45:47 -0400

    Fixed empty variable problem in tmux color script

--- tmux_color_setup.sh
+++ tmux_color_setup.sh
@@ -4,21 +4,23 @@ TMUX_LIGHTTEXTCOLOR="colour231"
 TMUX_DARKTEXTCOLOR="colour233"
 TMUX_LIGHTCOLORS=(3 7 10 11 14 15 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122 123 135 137 138 139 140 141 142 143 144 145 146 147 148 149 150 151 152 153 154 155 156 157 158 159 171 172 173 174 175 176 177 178 179 180 181 182 183 184 185 186 187 188 189 190 191 192 193 194 195 201 202 203 204 205 206 207 208 209 210 211 212 213 214 215 216 217 218 219 220 221 222 223 224 225 226 227 228 229 230 231 247 248 249 250 251 252 253 254 255)
 # Set the "hostcolor" based on a hash of the hostname
-tmux set-environment -g TMUX_HOSTCOLOR $(hostname | sum | awk '{print 1 + ($1 % 255)}')
+TMUX_HOSTCOLOR=$(hostname | sum | awk '{print 1 + ($1 % 255)}')
+tmux set-environment -g TMUX_HOSTCOLOR "${TMUX_HOSTCOLOR}"
 
 tmux set-environment -g TMUX_INACTIVE "${TMUX_INACTIVE}"
 tmux set-environment -g TMUX_BG "${TMUX_BG}"
 tmux set-environment -g TMUX_LIGHTTEXTCOLOR "${TMUX_LIGHTTEXTCOLOR}"
 tmux set-environment -g TMUX_DARKTEXTCOLOR "${TMUX_DARKTEXTCOLOR}"
-tmux set-environment -g TMUX_ACTIVE "colour$(tmux show-environment -g TMUX_HOSTCOLOR | sed 's/^TMUX_HOSTCOLOR=//')"
-tmux set-environment -g TMUX_TEXTCOLOR "${TMUX_LIGHTTEXTCOLOR}" # default
+TMUX_ACTIVE="colour$(tmux show-environment -g TMUX_HOSTCOLOR | sed 's/^TMUX_HOSTCOLOR=//')"
+tmux set-environment -g TMUX_ACTIVE "${TMUX_ACTIVE}"
 
 # define TMUX_TEXTCOLOR as light or dark based on TMUX_HOSTCOLOR (active color)
 if [[ " ${TMUX_LIGHTCOLORS[@]} " =~ " ${TMUX_HOSTCOLOR} " ]]; then
-    tmux set-environment -g TMUX_TEXTCOLOR ${TMUX_DARKTEXTCOLOR}
+    TMUX_TEXTCOLOR="${TMUX_DARKTEXTCOLOR}"
 else 
-    tmux set-environment -g TMUX_TEXTCOLOR ${TMUX_LIGHTTEXTCOLOR}
+    TMUX_TEXTCOLOR="${TMUX_LIGHTTEXTCOLOR}"
 fi
+tmux set-environment -g TMUX_TEXTCOLOR "${TMUX_TEXTCOLOR}"
 
 # status bar colors
 tmux set -g status-fg $TMUX_TEXTCOLOR
