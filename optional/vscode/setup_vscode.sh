4fbf704a8c52e126ba1a975e0629d6374a644262
Sarah Scheffler <sscheffl@andrew.cmu.edu>
2026-04-26 16:29:57 -0400

    adding vscode settings sync

---
+++ optional/vscode/setup_vscode.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+DOTFILES=$HOME/dotfiles
+BACKUP=$HOME/backup_dotfiles
+VSCODE=$HOME/.config/Code/User
+mkdir -p "$BACKUP" "$VSCODE"
+
+
+for file in settings.json keybindings.json; do
+
+    # Check if file is missing
+    [ -f "$DOTFILES/optional/vscode/$file" ] || { echo "Source missing: $file"; continue; }
+
+    # Backup old file
+    [ -f "$VSCODE/$file" ] && mv "$VSCODE/$file" "$BACKUP"
+
+    # Link new file
+    ln -sfv "$DOTFILES/optional/vscode/$file" "$VSCODE/$file"
+done
+
